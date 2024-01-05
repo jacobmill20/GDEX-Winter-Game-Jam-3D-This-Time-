e33c95b5e9099109db0b681b1f9ec68909ba4391
Jake Hamilton <Hamilton456b@gmail.com>
2024-01-05 13:51:07 -0600

    idek man im trying

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> gifts;
+    public List<Material> giftMaterials;
+    public List<Material> bowMaterials;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+            Begin Random Generation Coroutine
+            Generate the random and sets values in place before the game starts
+            Possibly do black screen for a couple seconds to allow the game to process and begin
+         */
+        StartCoroutine(GeneratePresents());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator GeneratePresents()
+    {
+        /*
+         Use random to pick a tag from the list
+         Also use random to pick correct color for gift from list
+
+         **I DONT KNOW WHAT THE LOGIC IS FOR HOW THE GIFTS ARE ASSIGNED FROM THE KIDS LISTS OR HOW THAT'S GOING TO BE DISPLAYED**
+         */
+        
+        //Assign Tag to Present
+
+        //Start Game
+        yield return null;
     }
 
     public void CheckCorrect()
